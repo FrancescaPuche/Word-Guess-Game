7ed77be39a7e5f3f2da066b45ef5489ab07e20df
Francesca P <francescapuche@gmail.com>
2019-03-25 21:09:10 -0400

    Updated HTML, CSS and initial variables for JS

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,29 @@
+// Javascript for Hangman Game
+
+
+// Variables and Arrays
+
+var wordAnswers = ["monkey", "penguin", "platypus", "seahorse", "tiger"]; 
+var currentWord =""; 
+var charInWord = [];
+var lettersCorrect = [];
+var dashNumber = 0; 
+var wrongLetters = []; 
+var winCount = 0; 
+var renmainingGuesses = 10; 
+var lossCount = 0; 
+
+// Functions 
+
+// How the game will work
+
+function game () { 
+
+}
+
+// console testing 
+
+
+
+// running function 
+
